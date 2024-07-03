3faab09b7574f9a36a5d9db40f83acd457408ba5
Abu Talha <talhaliaqat94@gmail.com>
2024-07-03 16:56:32 +0200

    use Annoy library

--- app/main.cpp
+++ app/main.cpp
@@ -7,66 +7,20 @@ int main() {
     std::cout<<"Loading data"<< std::endl;
     dataset::LaserScanDataset laser_data(data_root);
 
-    int iters = 15;
-
+    dataset::LaserScanDataset::PointCloud src = laser_data[1];
+    dataset::LaserScanDataset::PointCloud target = laser_data[2];
+    std::cout<< "Length of src: " << src.size() << std::endl;
+    std::cout<< "Length of target: " << target.size() << std::endl;
     
-    std::cout << "Implementing ICP"<< std::endl;
-    int last_precent = -5;
-    for (int i = 0; i < iters; i++)
-    {
-    int percent = (i * 100) / iters;
-    if (percent % 5 == 0 && percent != last_precent)
-    {
-        std::cout << percent << "% done" << std::endl;
-        last_precent = percent;
-    }
-    std::vector<Eigen::Vector2d> src = laser_data[i];
-    std::vector<Eigen::Vector2d> target = laser_data[i+1];
-    std::pair<Eigen::Matrix2d, Eigen::Vector2d> transformation = implement_icp(src, target);
-    laser_data.SetTransformation(transformation);
-    }
-    
-    std::cout << "Applying Transformations"<< std::endl;
-    last_precent = -5;
+    // Sample the source point cloud
+    dataset::LaserScanDataset::PointCloud sampled_src;
+    sampled_src = sample_points(src, 300);
+    std::cout<< "Length of sampled_src: " << sampled_src.size() << std::endl;
 
-    dataset::LaserScanDataset::PointCloud src = laser_data[0];
-    dataset::LaserScanDataset::PointCloud target = laser_data[1];
-    // std::cout<<"Points in first src scan: "<<src.size()<<std::endl;
-    // std::cout<<"Points in first target scan: "<<target.size()<<std::endl;
-    std::pair<Eigen::Matrix2d, Eigen::Vector2d> recovered_transformation = laser_data.GetTransformation(0);
-    dataset::LaserScanDataset::PointCloud transformed_src = apply_transformation(recovered_transformation, src);
-    transformed_src.reserve(transformed_src.size() + target.size());
-    transformed_src.insert(transformed_src.end(), target.begin(), target.end());
-    // std::cout<<"Points in first transformed scan: "<<transformed_src.size()<<std::endl;
-    laser_data.AddRegisteredPointCloud(transformed_src);
-    transformed_src.clear();
-    for (int i = 1; i < iters; i++)
-    {
-    int percent = (i * 100) / iters;
-    if (percent % 5 == 0 && percent != last_precent)
-    {
-        std::cout << percent << "% done" << std::endl;
-        last_precent = percent;
-    }
-    // std::cout<<"Iteration : "<<i<<std::endl;
-    dataset::LaserScanDataset::PointCloud registered_pcd = laser_data.GetRegisteredPointCloud();
-    // std::cout<<"Points in recovered scan: "<<registered_pcd.size()<<std::endl;
-    dataset::LaserScanDataset::PointCloud target = laser_data[i+1];
-    // std::cout<<"Points in target scan: "<<target.size()<<std::endl;
-    std::pair<Eigen::Matrix2d, Eigen::Vector2d> recovered_transformation = laser_data.GetTransformation(i);
-    dataset::LaserScanDataset::PointCloud transformed_src = apply_transformation(recovered_transformation, registered_pcd);
-    // std::cout<<"Points in transformed scan: "<<transformed_src.size()<<std::endl;
-    transformed_src.reserve(transformed_src.size() + target.size());
-    transformed_src.insert(transformed_src.end(), target.begin(), target.end());
+    // Find nearest neighbors using Annoy
+    dataset::LaserScanDataset::PointCloud correspondences = find_nearest_neighbours(sampled_src, target);
+    std::cout<< "Length of correspondences: " << correspondences.size() << std::endl;
 
-    // std::cout<<"Points in transformed scan + target scan: "<<transformed_src.size()<<std::endl;
-    laser_data.SetRegisteredPointCloud(transformed_src);
-    transformed_src.clear();
-    }
-
-
-    dataset::LaserScanDataset::PointCloud recovered_pcd = laser_data.GetRegisteredPointCloud();
-    // std::cout<<"Points in combined pcd:" << recovered_pcd.size()<< std:: endl;
-    viewCloud(recovered_pcd);
     return 0;
 }
+
--- viewer/viewer.cpp
+++ viewer/viewer.cpp
@@ -6,6 +6,10 @@
 #include <open3d/core/ShapeUtil.h>
 #include <utility>
 #include <vector>
+#include "../annoy/src/annoylib.h"      // for annoy::AnnoyIndex
+#include "../annoy/src/kissrandom.h"
+#include <algorithm>                    // for std::ranges::sample
+#include <random>                       // for std::mt19937
 
 namespace
 {
@@ -31,6 +35,8 @@ namespace
     double error(const std::vector<Eigen::Vector2d> &src, const std::vector<Eigen::Vector2d> &target, const Eigen::Matrix2d &R, const Eigen::Vector2d &t)
     {
         double error = 0.0;
+        // std::for_each(src.begin(), src.end(), [R, t](Eigen::Vector2d &point) { point = R * point + t; });
+        // error = (src - target).norm();
         for (size_t i = 0; i < src.size(); i++)
         {
             error += (R * src[i] + t - target[i]).norm();
@@ -59,13 +65,15 @@ void viewCloud(std::vector<Eigen::Vector2d> &first, const std::vector<Eigen::Vec
 }
 
 
-std::pair<Eigen::Matrix2d, Eigen::Vector2d> implement_icp(std::vector<Eigen::Vector2d> &src, const std::vector<Eigen::Vector2d> &target)
+std::pair<Eigen::Matrix2d, Eigen::Vector2d> icp_known_correspondence(std::vector<Eigen::Vector2d> &src, const std::vector<Eigen::Vector2d> &target)
 {   
     Eigen::Vector2d x0 = Eigen::Vector2d::Zero();
     Eigen::Vector2d y0 = Eigen::Vector2d::Zero();
     Eigen::Matrix2d H = Eigen::Matrix2d::Zero();
     Eigen::Matrix2d R = Eigen::Matrix2d::Identity();
     Eigen::Vector2d t = Eigen::Vector2d::Zero();
+    Eigen::Matrix2d R_ = Eigen::Matrix2d::Identity();
+    Eigen::Vector2d t_ = Eigen::Vector2d::Zero();
     int count = 1;
     while (true)
      {
@@ -80,21 +88,79 @@ std::pair<Eigen::Matrix2d, Eigen::Vector2d> implement_icp(std::vector<Eigen::Vec
         Eigen::Matrix2d R = U * V.transpose();
         Eigen::Vector2d t = y0 - R * x0;
 
-        std::for_each(src.begin(), src.end(), [R, t](Eigen::Vector2d &point) { point = R * point + t;});
+        R_ = R * R_;
+        t_ = t_ + t;
 
         count++;
 
-        if (error(src, target, R, t) <  0.0001 || count > 1000)
+        if (error(src, target, R_, t_) <  70 || count == 2)
         {   
+            // std::cout<<"Count: "<<count<<std::endl;
             break;
         }
     }
-    return std::make_pair(R, t);
+    return std::make_pair(R_, t_);
 }
+
+// std::pair<Eigen::Matrix2d, Eigen::Vector2d> icp_unknown_correspondence(std::vector<Eigen::Vector2d> &src, const std::vector<Eigen::Vector2d> &target){
+// int max_iterations = 1000;
+// double max_err = 1e-5;
+
+
+// // neighbour = NearestNeighbors(n_neighbors=1).fit(Line1.T)
+// //     for i in range(MaxIter):
+
+// //         # find correspondences of points
+// //         QInd = np.array([i[0] for i in neighbour.kneighbors(Line2.T, return_distance=False)]) 
+// //         PInd = np.arange(Line2.shape[1])
+
+// //         # update Line2 and error
+// //         # Now that you know the correspondences, use your implementation
+// //         # of icp with known correspondences and perform an update
+// //         EOld = E
+// //         [Line2, E] = icp_known_corresp(Line1, Line2, QInd, PInd)
+
+// //         print('Error value on ' + str(i) + ' iteration is: ', E)
+
+// //         # TODO: perform the check if we need to stop iterating
+// //         if E - EOld == 0 or E < Epsilon:
+// //             break
+        
+// //     # return the final error 
+// //     return E
+
+// }
+
 
 std::vector<Eigen::Vector2d> apply_transformation(const std::pair <Eigen::Matrix2d, Eigen::Vector2d> &transformation, const std::vector<Eigen::Vector2d> &src){
     std::vector<Eigen::Vector2d> transformed(src.size());
     // std::for_each(src.begin(), src.end(), [transformation, transformed](Eigen::Vector2d &point) { point = transformation.first * point + transformation.second; });
     transformed = src;
     return transformed;
+}
+
+std::vector<Eigen::Vector2d> sample_points(const std::vector<Eigen::Vector2d> &vec, int num_samples) {
+    std::vector<Eigen::Vector2d> sampled_vec;
+    std::sample(vec.begin(),vec.end(),std::back_inserter(sampled_vec) ,num_samples, std::mt19937 {std::random_device{}()});
+    return sampled_vec;
+}
+
+std::vector<Eigen::Vector2d> find_nearest_neighbours(const std::vector<Eigen::Vector2d> &src, const std::vector<Eigen::Vector2d> &target) {
+    std::vector<Eigen::Vector2d> nearest_neighbours;
+    int dimension = 2;
+    int n_points = target.size();
+    int n_trees = 10;
+    
+    Annoy::AnnoyIndex<int, double, Annoy::Euclidean, Annoy::Kiss64Random,Annoy::AnnoyIndexSingleThreadedBuildPolicy> annoy_index(dimension);
+
+    for (int i = 0; i < n_points; i++) {annoy_index.add_item(i, target[i].data());}
+
+    annoy_index.build(n_trees, -1);
+
+    std::vector<int> indices;
+    for (const auto &src_vec : src) { annoy_index.get_nns_by_vector(src_vec.data(), 1, -1, &indices, nullptr); }
+
+    for (int idx : indices) { nearest_neighbours.push_back(target[idx]); }
+    
+    return nearest_neighbours;
 }
